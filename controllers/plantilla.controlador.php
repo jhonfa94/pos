8d3044a53dcd70768fadcccef5c9c5754cbae80a
Fabio Cardona <synapsis.developer1@outlook.com>
2019-12-02 22:08:44 -0500

    iniciando con la modularizacion

---
+++ controllers/plantilla.controlador.php
@@ -0,0 +1,11 @@
+<?php
+
+class ControladorPlantilla { 
+
+    function ctrPlantilla(){
+        
+        include 'views/plantilla.php';
+
+    }
+
+}
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 
+require_once 'controllers/plantilla.controlador.php'; 
+
+$plantilla = new ControladorPlantilla();
+$plantilla->ctrPlantilla();
 
